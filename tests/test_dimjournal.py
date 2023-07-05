4da8efd540a964f24349fb0ad767617080f384b1
twardoch <adam@twardoch.com>
2023-07-05 03:01:00 +0200

    Update test_dimjournal.py

--- tests/test_dimjournal.py
+++ tests/test_dimjournal.py
@@ -8,38 +8,5 @@ from pathlib import Path
 import undetected_chromedriver as webdriver
 
 
-# Mocking the webdriver for testing
-class MockDriver:
-    def __init__(self):
-        pass
-
-    def add_cookie(self, cookie):
-        pass
-
-    def get(self, url):
-        pass
-
-    def get_cookie(self, name):
-        return {"value": "test_value"}
-
-    def quit(self):
-        pass
-
-
-@pytest.fixture
-def api():
-    return MidjourneyAPI(driver=MockDriver(), archive_folder=Path.cwd())
-
-
-@pytest.fixture
-def crawler(api):
-    return MidjourneyJobCrawler(api, Path.cwd(), job_type="upscale")
-
-
-@pytest.fixture
-def downloader(api):
-    return MidjourneyDownloader(api, Path.cwd())
-
-
-def test_midjourney_downloader_read_jobs(downloader):
+def test():
     assert True
